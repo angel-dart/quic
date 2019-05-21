47600bf13f33703b77403017a2d487de2c420835
Tobe O <thosakwe@gmail.com>
2019-05-21 10:25:45 -0400

    Stream type

---
+++ quic/lib/src/stream_type.dart
@@ -0,0 +1,7 @@
+/// The various types of QUIC streams.
+enum StreamType {
+  clientInitiatedBidirectional,
+  serverInitiatedBidirectional,
+  clientInitiatedUnidirectional,
+  serverInitiatedUnidirectional,
+}
