1e428ed414e32d0c054a3c99106657a1a4ade509
Tobe O <thosakwe@gmail.com>
2019-05-21 10:33:04 -0400

    Basic frame types, etc.

--- quic/lib/src/frame.dart
+++ quic/lib/src/frame.dart
@@ -1,1 +1,13 @@
-
+/// A QUIC protocol stream.
+abstract class Frame {
+  static const int paddingType = 0x00,
+      pingType = 0x01,
+      ackWithSumType = 0x03,
+      ackType = 0x02,
+      resetStreamType = 0x04,
+      stopSendingType = 0x05,
+      cryptoType = 0x06,
+      newTokenType = 0x07,
+      maxDataType = 0x10,
+      maxStreamDataType = 0x11;
+}
---
+++ quic/lib/src/unidirectional_stream_state.dart
@@ -0,0 +1,3 @@
+/// A state machine that represents the status of a
+/// unidirectional stream.
+class UnidirectionalStreamState {}
